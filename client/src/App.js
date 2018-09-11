9d15c6c008d6d52119c14b58f195df5f78647ca9
Joshua Wood <joshua@Joshuas-iMac.local>
2018-09-10 20:59:52 -0400

    bootstrap and example gists

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Gist from 'super-react-gist';
+import './components/css/gist.css'
 
 class App extends Component {
   state = {
@@ -25,9 +27,21 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="text-center jumbotron">React Coding Challenges!</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        {/* <p className="text-center">{this.state.response}</p> */}
+        <div className='col-12 col-md-6'>
+          <button style={{width: "100%"}} classNam="btn"type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">PythonGist</button>
+          <div style={{margin: '10px 0 10px 0'}} className="collapse" id="collapseExample">
+            <Gist url="https://gist.github.com/JoshTRN/cda9530e9a905e26c978253984fb47eb" />
+          </div>
+        </div>
+        <div className='col-12 col-md-6'>
+          <button style={{width: "100%"}} classNam="btn"type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">PythonGist</button>
+          <div style={{marginTop: '10px'}} className="collapse" id="collapseExample2">
+            <Gist url="https://gist.github.com/JoshTRN/cda9530e9a905e26c978253984fb47eb" />
+          </div>
+        </div>
       </div>
     );
   }
